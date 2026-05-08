769bdc60362ba24cbabed6e88d2fcafbedef4300
Ricardo Quispe C. <ricardo.quispe@dinet.com.pe>
2026-05-08 18:24:57 -0500

    Integración exitosa con MongoDB

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
+require('dotenv').config(); // Carga las variables secretas del archivo .env
 const express = require('express');
+const mongoose = require('mongoose'); // Importamos Mongoose
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Permite que Express entienda los datos en formato JSON
+app.use(express.json()); 
+
+// 1. Conexión a MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('🟢 Conectado exitosamente a MongoDB Atlas');
+    
+    // 2. Encendemos el servidor SOLO si hay base de datos
+    app.listen(port, () => {
+      console.log(`🚀 Servidor de Ricart Labs corriendo en el puerto ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error('🔴 Error conectando a la base de datos:', error);
+  });
+
+// 3. Tus Rutas
 app.get('/', (req, res) => {
   res.json({
     status: 'online',
+    database: 'connected',
     message: 'Ricart Labs API está operando',
-    version: '1.0.0'
   });
-});
-
-app.listen(port, () => {
-  console.log(`Servidor corriendo en el puerto ${port}`);
 });
